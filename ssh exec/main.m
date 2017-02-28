19e455b40c0f4f414cfd4cbb97064d051d5dd2da
Benjamin Rannow <b.rannow@familie-redlich.de>
2017-02-28 11:52:55 +0100

    first commit

---
+++ ssh exec/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ssh exec
+//
+//  Created by Benjamin on 28.02.17.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
